3f4791a338cddfde3955d8b96b84cea7405a4252
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-07-19 09:13:40 -0700

    Update lic-renew.sh

--- lic-renew.sh
+++ lic-renew.sh
@@ -35,7 +35,8 @@ if [ -z "$cr_dir" ]; then
 fi
 
 export DEV_ID=$lic_id
-export CSR_ID=$(git log --pretty=oneline --abbrev-commit -- ${cr_dir} | grep -Po '[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}')
+CSR_ID=$(git log --pretty=oneline --abbrev-commit -- ${cr_dir} | grep -Po '[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}')
+export CSR_ID=$(echo ${CSR_ID} | cut -d' ' -f 1)
 export REQ_EMAIL=$(openssl req -in ./csr-list/$CSR_ID.csr -noout -text | grep -Po '([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)')
 printf "\n\n**** Req Email: %s\n\n" "$REQ_EMAIL"
 
